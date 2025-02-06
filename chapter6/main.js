f65b9f6213d58b22fc53cfc41d934942dddd3a6b
Aidan-Bobs <aidankaroberts@gmail.com>
2025-02-06 09:26:01 +0200

    Chapter 5 push

---
+++ chapter6/main.js
@@ -0,0 +1,32 @@
+var family = ["mom", "dad","bro", "sis"]
+
+/* var myname = "Ai";
+
+var surname = "Roberts"
+if (myname.length >= 5 && surname.length >= 5) {
+    console.log("You have a really long name and surname!!")
+} else if(myname.length === 2) {
+    console.log("I guess its too short")
+}
+else {
+    console.log("bruh")
+} */
+
+
+
+/* var sheepCounted = 0;
+while (sheepCounted < 10) {
+    console.log("I have counted " + sheepCounted + " sheep!");
+    sheepCounted++;
+}
+console.log("Zzzzzzzzzzz"); */
+
+/* for (var i = 0; i < 3; i++) {
+    console.log("I have counted" + i + " vars.")
+}; */
+
+
+
+for (var i = 0; i < family.length; i++) {
+    console.log("This is my family member " + family[i] + ".");
+}
