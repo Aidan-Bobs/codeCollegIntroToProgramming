de81abec43979682830b7b795e1a02a49430503c
Aidan-Bobs <aidankaroberts@gmail.com>
2025-02-12 09:03:44 +0200

    idk

---
+++ dummies/chatper11/main.js
@@ -0,0 +1,24 @@
+let parents = [
+    "Father",
+    "Mother"
+]
+
+let friends =[
+    "Aerin",
+    "Rudy"
+]
+
+let pets = [
+    "cat",
+    "dog"
+]
+
+let family = friends.concat(parents, pets)
+
+
+
+let longArr = [1,2,3,4,5,6,7,8,9]
+
+let arr2 = []
+
+arr2.push(longArr.slice(2, 4))
